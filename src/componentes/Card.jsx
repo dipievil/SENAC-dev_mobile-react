49b2e58672edc7f3ae4e1fb50852860354be8c5b
Diego Ritzel <dipi.evil@gmail.com>
2022-04-02 09:50:41 -0300

    Card adjust

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 
 function Card(props) {
     return (
-    <div className="card"> 
-        <div className="card-titulo">
-            <h1>{props.titulo}</h1>
+        <div class="col-6 col-md-4">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <img class="card-img-top" src={props.img} alt={props.nome} />
+                    <h5 class="card-title">{props.nome}</h5>
+                    <p class="card-text">{props.desc}</p>
+                    <a href="#" class="btn btn-primary">{props.preco}</a>
+                </div>
+            </div>
         </div>
-        <div className="card-conteudo">
-            {props.children}
-        </div>
-    </div>
     )
 }
 
